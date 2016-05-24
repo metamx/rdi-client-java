81415251904da88d931eadc96e5007f75e599603
Neel Sharma <neel.sharma@metamarkets.com>
2016-05-24 11:33:22 -0700

    Update jenkins-deploy.sh

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -10,5 +10,5 @@ DIST_TAR=${ARTIFACT}-distribution-${TAG}-dist.tar.gz
 
 S3_PATH=${S3_BUCKET}/${ARTIFACT}/${TAG}
 
-aws s3 mv target/site ${S3_PATH}/site --recursive --acl bucket-owner-full-control
-aws s3 mv distribution/target/${DIST_TAR} ${S3_PATH} --acl bucket-owner-full-control
+aws s3 mv target/site ${S3_PATH}/site/ --recursive --acl bucket-owner-full-control
+aws s3 mv distribution/target/${DIST_TAR} ${S3_PATH}/ --acl bucket-owner-full-control
